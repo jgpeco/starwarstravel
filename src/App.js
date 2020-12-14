4d6f8b92da2888e6b4dc043b8a09638f0f6c564c
Gabriel <joaogabrielonofre@gmail.com>
2020-12-14 14:52:11 -0300

    Create first interactions with the API in the App component itself, make App fetch data from swapi.dev

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
 
-function App() {
+const Ship = ({shipInfo}) => {
   return (
     <div>
-      <header>
-        Well, hi
-      </header>
+      <p>Name: {shipInfo.name}</p>
+      <p>MGLT: {shipInfo.MGLT}</p>
+      <p>Consumables Time: {shipInfo.consumables}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const [allShips, setAllShips] = useState([])
+  const [nextButton, setNextButton] = useState('')
+  const [prevButton, setPrevtButton] = useState('')
+
+  const getAllShips = async () => {
+    const response = await axios.get(`https://swapi.dev/api/starships/?page=2`)
+    const { data } = response
+    
+    const ships = data.results
+    setAllShips(ships)
+
+    if(data.previous) setPrevtButton(data.previous)
+    if(data.next) setNextButton(data.next)
+  }
+
+  const handleGetButton = () => {
+    getAllShips()
+  }
+
+  const handlePrevButton = () => {
+    console.log('hi from prev button')
+  }
+
+  const handleNextButton = () => {
+    console.log('hi from next button')
+  }
+
+  return (
+    <div>
+      <button onClick={handleGetButton}>Click to get all the ships</button>
+      { allShips ?
+          allShips.map(ship => 
+            <Ship key={ship.url} shipInfo={ship} />
+          ) :
+          null
+      }
+      { prevButton ? <button onClick={handlePrevButton}>Previous Ships</button> : null }
+      { nextButton ? <button onClick={handleNextButton}>Next Ships</button> : null }
     </div>
   );
 }
