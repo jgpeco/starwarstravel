2efac20a44ac99049c12e54f17967c5afe3913b4
Gabriel <joaogabrielonofre@gmail.com>
2020-12-14 16:49:39 -0300

    Create function to calculate distance. Insert input field to receive the distance from the user

--- src/App.js
+++ src/App.js
@@ -12,8 +12,11 @@ const Ship = ({shipInfo}) => {
 }
 
 const App = () => {
+  //user input (mglts)
+  const [distance, setDistance] = useState(0)
+  //fetch ships
   const [allShips, setAllShips] = useState([])
-  const [page, setPage] = useState(0)
+  const [page, setPage] = useState(1)
   const [nextButton, setNextButton] = useState(false)
   const [prevButton, setPrevButton] = useState(false)
 
@@ -53,8 +56,57 @@ const App = () => {
     setPage(currPage => currPage + 1)
   }
 
+  const calculateStops = (ship) => {
+    const mglt = Number(ship.MGLT)
+    const hoursToArrive = distance / mglt
+
+    const consumables = ship.consumables
+
+    //gets just the number of days, weeks, months or years. it will be converted below
+    const consumablesQuantity = Number(consumables.split(' ')[0])
+    let convert = 0
+    
+    //converts the consumable quantity to hours
+    if(ship.consumables && ship.consumables !== 'unknown'){
+      if(consumables.includes('day')) convert = 24
+      if(consumables.includes('week')) convert = 168
+      if(consumables.includes('month')) convert = 720
+      if(consumables.includes('year')) convert = 8760
+    } else {
+      return 'Unknown'
+    }
+
+    const consumablesInHours = consumablesQuantity * convert
+    return Math.round(hoursToArrive / consumablesInHours)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const response = await axios.get(`https://swapi.dev/api/starships/?page=${page}`)
+    const { data } = response
+    
+    const ships = data.results
+    const shipsWithStops = ships.map(ship => ship.stops = calculateStops(ship))
+
+    setAllShips(ships)
+  }
+
   return (
     <div>
+      <form onSubmit={handleSubmit}>
+        <p>Type the travel distance in MegaLights (MGLT)</p>
+        <input
+              id='input-distance'
+              name='distance'
+              placeholder='Type the distance...'
+              value={distance}
+              onChange={({ target }) => setDistance(target.value)}
+          />
+          <button type='submit'>Calculate!</button>
+      </form>
+        
+
       { allShips.length ?
           allShips.map(ship => 
             <Ship key={ship.url} shipInfo={ship} />
